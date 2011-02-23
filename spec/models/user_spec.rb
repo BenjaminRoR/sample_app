0dfa9d55fd8a419286f2942cf5e060c32a09394c
BenjaminRoR <benjamin.amir@gmx.ch>
2011-02-23 18:00:56 +0100

    test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should create a new instance given a valid attribute" do
+    User.create!(:name => "Example User", :email => "user#example.net")
+  end
 end
